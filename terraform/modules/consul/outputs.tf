6189c415957d9ebfd0bdee90d7167cac9bcaabe4
Jan-Christoph Kuester <jckuester@gmail.com>
2017-07-06 17:04:12 +0200

    Remove consul node output

--- terraform/modules/consul/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "consul0_private_ip" {
-  value = "${aws_instance.consul.0.private_ip}"
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,3 @@ output "elb_dns_name" {
 output "bastion_public_ip" {
   value = "${module.bastion.public_ip}"
 }
-
-output "consul0_private_ip" {
-  value = "${module.consul.consul0_private_ip}"
-}
