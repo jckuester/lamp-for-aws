37f76718d4e1545c4aaed3f2ebf735603ec7cec8
Jan-Christoph Kuester <jckuester@gmail.com>
2017-07-06 18:06:57 +0200

    Update master slave switch script

--- terraform/modules/vpc/security.tf
+++ terraform/modules/vpc/security.tf
@@ -19,4 +19,10 @@ resource "aws_security_group" "consul_gossip" {
       "${var.vpc_cidr}"
     ]
   }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
